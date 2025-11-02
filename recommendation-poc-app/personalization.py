aecf1e54f611b0bf439b9f2f0ee16591c78e4967
Rajesh <rajesh.kumar@kitab.com>
2025-11-02 15:09:56 +0530

    page setting updates

--- recommendation-poc-app/personalization.py
+++ recommendation-poc-app/personalization.py
@@ -21,7 +21,7 @@ st.set_page_config(
     page_title="Personalized Recommendations",
     page_icon="✨",
     layout="wide",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="expanded"
 )
 
 
